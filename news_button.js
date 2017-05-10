638fad7de32379f37dd8c95659e838710f235be2
Alexandra <sakujo8744@mail.ru>
2017-05-10 17:23:31 +0300

    news buttons behave

--- news_button.js
+++ news_button.js
@@ -1,7 +1,45 @@
+function press(elem){
+  if (elem["pressed"] == undefined){
+  	elem.pressed = false;
+  } 
+  elem.pressed = !elem.pressed;
+  if (elem.pressed) push(elem);
+  else unpush(elem);
+}
 
 function push(elem){
   elem.style.backgroundColor = "#c33";
   elem.style.color = "#fff";
-  elem.innerHTML = "Вы пойдёте"
+  elem.innerHTML = "Вы пойдёте";
+}
+
+function unpush(elem){
+  elem.style.backgroundColor = "#fff";
+  elem.style.color = "#c33";
+  elem.innerHTML = "Я пойду!";
+}
+
+function hover(elem){
+  if (elem.pressed) {
+  	elem.style.backgroundColor = "#d44";
+    elem.style.color = "#fff";
+    elem.innerHTML = "Не пойду";
+  } else {
+  	  elem.style.backgroundColor = "#b11";
+      elem.style.color = "#fff";
+      elem.innerHTML = "Я пойду!";
+  }
+}
+
+function unhover(elem){
+if (elem.pressed) {
+  elem.style.backgroundColor = "#b11";
+  elem.style.color = "#fff";
+  elem.innerHTML = "Вы пойдёте";
+  } else {
+  	 elem.style.backgroundColor = "#fff";
+     elem.style.color = "#b22";
+     elem.innerHTML = "Я пойду!";
+  }
 }
 
