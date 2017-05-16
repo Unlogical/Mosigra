76527c59a29e10b8a3fe1990f1c713fe8d82065b
Alexandra <sakujo8744@mail.ru>
2017-05-16 18:20:47 +0300

    userpage progress colors nojs added

---
+++ progress.js
@@ -0,0 +1,16 @@
+/*window.onload = function(){
+  var flag = 0;
+  var elements = document.getElementsByClassName('red');
+  for (var i=0; i<elements.length; i++)  {
+  	var j = i;
+  	j = j - flag;
+  	if (elements[j].value > 66) {
+	  elements[j].className = elements[j].className.replace('red', 'green');
+	  flag++;
+	}
+	if (elements[j].value > 33 && elements[j].value<=66) {
+	  elements[j].className = elements[j].className.replace('red', 'yellow');
+	  flag++;
+	}
+  }
+}*/
